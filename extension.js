84c74849708c2725bc56de47ca5402235f0da7b3
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2023-01-04 13:20:26 +0530

    Introduce Test Extension 2

--- extension.js
+++ extension.js
@@ -12,16 +12,16 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-extension" is now active!');
+	console.log('Congratulations, your extension "test-extension-2" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('test-extension-2.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Test Extension!');
+		vscode.window.showInformationMessage('Hello World from Test Extension 2!');
 	});
 
 	context.subscriptions.push(disposable);
